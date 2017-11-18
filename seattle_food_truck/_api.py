10c25d2a1afc01a3ac3ed407bfaec9bda9628289
clintval <valentine.clint@gmail.com>
2017-11-18 00:33:50 -0800

    First round of documentation before testing and new features

--- seattle_food_truck/_api.py
+++ seattle_food_truck/_api.py
@@ -3,7 +3,7 @@ import datetime
 
 import requests
 
-from pandas import to_datetime
+from pandas import to_datetime  # Hope to get rid of this dependency soon!
 
 
 __all__ = [
@@ -12,10 +12,34 @@ __all__ = [
 
 
 class Location(object):
-    PI_BUILDING = '69'
+    """A listing of all buildings and their location codes."""
+    PI_building = '69'
 
 
 def humanize_list(array):
+    """Return a grammatically correct English phrase of a list of items. Any
+    list with items greater than 2 will, of course, use an Oxford comma.
+
+    Parameters
+    ----------
+    array : iterable
+        Any list of items with a __str__ method.
+
+    Returns
+    -------
+    phrase : str
+        A grammatically correct way of saying the iterable.
+
+    Examples
+    --------
+    >>> humanize_list(['tomato'])
+    'tomato'
+    >>> humanize_list(['tomato', 'cabbage'])
+    'tomato and cabbage'
+    >>> humanize_list(['tomato', 'cabbage', 'lettuce'])
+    'tomato, cabbage, and lettuce'
+
+    """
     new = list(map(str, array))
     if len(array) == 1:
         return ''.join(new)
@@ -37,6 +61,7 @@ class Truck():
 
     @property
     def food_description(self):
+        """Return a grammatically correct listing of categorical food items"""
         return humanize_list(self.food_categories)
 
     def __repr__(self):
@@ -47,22 +72,39 @@ class Truck():
 
 class SeattleFoodTruckAPI():
     def __init__(self, location):
+        # Hosting for the main domain and image hosting platform.
         self.host = 'https://www.seattlefoodtruck.com'
         self.img_host = ('https://s3-us-west-2.amazonaws.com/'
                          'seattlefoodtruck-uploads-prod')
+
+        # API keys and values needed to get truck bookings.
         self.options = {
             'for_locations': location,
             'with_active_trucks': 'true',
             'include_bookings': 'true',
             'with_booking_status': 'approved'}
 
-    # Consider using the field `total_pages` to grab all pages in the
-    # pagination so a user is guaranteed to find a food truck when using the
-    # ``trucks_on_day()`` method if one such glorious food truck exists.`
     def events_by_page(self, page):
-        options = '&'.join('='.join((k, v)) for k, v in self.options.items())
-        URL = f'{self.host}/api/events?page={page}&{options}'
-        response = requests.get(URL)
+        """Consider using the field `total_pages` to grab all pages in the
+        pagination so a user is guaranteed to find a food truck when using the
+        ``trucks_on_day()`` method if one such glorious food truck exists.
+
+        Parameters
+        ----------
+        page : int
+            The page to query from the paginator.
+
+        Returns
+        -------
+        events : dict
+            A JSON dictionary of all events on the requested page.
+
+        """
+        # Join all key values and concatenate them with ampersands
+        query = '&'.join('='.join((k, v)) for k, v in self.options.items())
+
+        # Make a GET request and parse the resulting call to JSON.
+        response = requests.get(f'{self.host}/api/events?page={page}&{query}')
         events = json.loads(response.text).get('events')
         return events
 
@@ -75,6 +117,7 @@ class SeattleFoodTruckAPI():
             # compatible with that format.
             scheduled_date = to_datetime(event.get('start_time')).date()
 
+            # Instantiate all trucks that have been booked on this event.
             if date == scheduled_date:
                 trucks.extend([Truck(booking['truck']) for
                                booking in event.get('bookings')])
@@ -82,14 +125,17 @@ class SeattleFoodTruckAPI():
         return trucks
 
     def trucks_today(self):
+        """Returns food trucks scheduled for today"""
         today = datetime.datetime.today().date()
         return self.trucks_on_day(today)
 
     def trucks_tomorrow(self):
+        """Returns food trucks scheduled for tomorrow"""
         today = datetime.datetime.today().date()
         return self.trucks_on_day(today + datetime.timedelta(days=1))
 
     def trucks_yesterday(self):
+        """Returns yesterdays food trucks so you can be sad you missed them"""
         today = datetime.datetime.today().date()
         return self.trucks_on_day(today - datetime.timedelta(days=1))
 
@@ -99,7 +145,6 @@ class SeattleFoodTruckAPI():
             f'location="{self.parameters["for_locations"]}")')
 
 
-if __name__ == '__main__':
-    # Usage!
-    api = SeattleFoodTruckAPI(Location.PI_BUILDING)
+if __name__ == '__main__':  # Usage... will remove in favor of scripts/xxx.py
+    api = SeattleFoodTruckAPI(Location.PI_building)
     print(api.trucks_today())
