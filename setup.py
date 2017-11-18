3006a2c78874e92b5b7b104feb2dd25492738c0b
clintval <valentine.clint@gmail.com>
2017-11-18 00:33:38 -0800

    Added setup.py installer

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seattle_food_truck',
+    version='0.1',
+    author='clintval',
+    author_email='valentine.clint@gmail.com',
+    url='https://github.com/clintval/seattle-food-truck',
+    license='MIT',
+    install_requires=[
+        'pandas',
+        'requests'],
+    scripts=[],  # CLI coming!
+    packages=find_packages())
+
+print("""
+-------------------------------------------
+ seattle_food_truck installation complete!
+-------------------------------------------
+""")
